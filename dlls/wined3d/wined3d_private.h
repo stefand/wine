d9659b5759f271f9fcb3314cc60ac7781632a4c3
Stefan Dösinger <stefan@codeweavers.com>
2013-07-30 22:17:10 +0200

    wined3d: Separate GL buffer discard control from ignoring MAP_DISCARD.
    Ignoring WINED3D_MAP_DISCARD is controlled purely in the main thread,
    whereas the other one is controlled in the worker thread. Store them in
    separate fields for synchronization purposes.
    
    There are some problems left:
    * buffer->flags is still accessed by both threads without sync.
    * CSMT=off has a separate codepath that is untested.

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -2962,6 +2962,7 @@ struct wined3d_buffer
     GLenum buffer_object_usage;
     GLenum buffer_type_hint;
     DWORD flags;
+    BOOL ignore_discard;
     void *map_ptr;
 
     struct wined3d_map_range *maps;
@@ -2989,6 +2990,7 @@ void buffer_internal_preload(struct wined3d_buffer *buffer, struct wined3d_conte
 HRESULT wined3d_buffer_upload_data(struct wined3d_buffer *buffer,
         const struct wined3d_box *box, const void *data) DECLSPEC_HIDDEN;
 void buffer_invalidate_bo_range(struct wined3d_buffer *This, UINT offset, UINT size) DECLSPEC_HIDDEN;
+void buffer_swap_mem(struct wined3d_buffer *buffer, BYTE *mem) DECLSPEC_HIDDEN;
 
 struct wined3d_rendertarget_view
 {
